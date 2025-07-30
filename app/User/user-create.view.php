138bd0d86eac75c2a8271c547ba6b226fc5a2cf9
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2025-07-31 04:00:28 +0900

    Update user-create.view.php

--- app/User/user-create.view.php
+++ app/User/user-create.view.php
@@ -1,9 +1,21 @@
+<?php
+use App\User\UserController;
+use function Tempest\uri;
+?>
+
 <x-base :title="'Create User'">
-    <main class="w-screen h-screen overflow-hidden bg-sky-100/20">
-        <form action="/users" method="POST">
-            <input type="text" name="name" placeholder="Name">
-            <input type="email" name="email" placeholder="Email">
-            <button type="submit">Create</button>
-        </form>
-    </main>
+    <h1 class="m-4 text-lg text-center">Create User</h1>
+    <div class="mr-4 ml-4">
+        <x-form :action="uri([UserController::class, 'store'])" :method="'POST'">
+            <x-input class="px-4 py-2 w-full"
+                :label="'Name: '" :type="'text'" :name="'name'" :placeholder="'John Doe'" />
+
+            <x-input class="px-4 py-2 w-full"
+                :label="'Email: '" :type="'email'" :name="'email'" :placeholder="'sample@example.com'" />
+
+            <div class="flex justify-center mb-8">
+                <x-submit class="bg-blue-500 text-white px-4 py-2 rounded" :label="'Create'" />
+            </div>
+        </x-form>
+    </div>
 </x-base>
