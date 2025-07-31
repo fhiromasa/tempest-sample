9fbb54c73e17a5f274db89f928808d3530fc3a72
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2025-07-31 17:13:01 +0900

    [ci]fix lint

--- app/User/Store/UserStoreRquest.php
+++ app/User/Store/UserStoreRquest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\User\Store;
 
 use Tempest\Http\IsRequest;
--- app/User/user-create.view.php
+++ app/User/user-create.view.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\User\UserController;
 use Tempest\Http\Session\Session;
 use Tempest\Validation\Rule;
