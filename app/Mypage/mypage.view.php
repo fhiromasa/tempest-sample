21ec08af78e49acff389c75813158076d17b4928
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2026-03-11 11:23:52 +0900

    feat: show user data in mypage.

--- app/Mypage/mypage.view.php
+++ app/Mypage/mypage.view.php
@@ -2,5 +2,7 @@
     <div class="container">
         <h1>Welcome to My Page</h1>
         <p>This is your personalized page.</p>
+        <p><strong>ID:</strong> {{ $id }}</p>
+        <p><strong>Email:</strong> {{ $email }}</p>
     </div>
 </x-base>
