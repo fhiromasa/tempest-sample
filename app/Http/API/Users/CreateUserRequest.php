274fd8126ec02f4d2b110fd4b4ada0a2165b276d
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2026-04-27 01:04:42 +0900

    imple crud api for users.

---
+++ app/Http/API/Users/CreateUserRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\API\Users;
+
+use Tempest\Http\IsRequest;
+use Tempest\Http\Request;
+use Tempest\Http\SensitiveField;
+use Tempest\Validation\Rules\HasLength;
+
+final class CreateUserRequest implements Request
+{
+    use IsRequest;
+
+    #[HasLength(min: 3, max: 100)]
+    public string $username;
+
+    #[HasLength(min: 3, max: 100)]
+    public string $email;
+
+    #[HasLength(min: 3, max: 100), SensitiveField]
+    public string $password;
+}
