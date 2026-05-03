2807d564946b02dcc3648c9d43be88dd4993d7ef
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2026-05-04 02:03:41 +0900

    manually update: logs.config.php

--- app/Config/logs.config.php
+++ app/Config/logs.config.php
@@ -2,19 +2,9 @@
 
 declare(strict_types=1);
 
-use Tempest\Log\Channels\AppendLogChannel;
 use Tempest\Log\Channels\DailyLogChannel;
-use Tempest\Log\LogConfig;
 
-return new LogConfig(
-    channels: [
-        new AppendLogChannel(
-            path: __DIR__ . '/../../log/project.log',
-        ),
-        new DailyLogChannel(
-            path: __DIR__ . '/../../log/daily.log',
-            maxFiles: 10,
-        ),
-    ],
-    debugLogPath: __DIR__ . '/../../log/debug.log',
+return new DailyLogChannel(
+    path: __DIR__ . '/../../log/daily.log',
+    maxFiles: 10,
 );
