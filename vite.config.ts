7bc7c01e51639de280a4bfe80a62263772338ee3
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2026-05-23 20:05:06 +0900

    set server.host=true in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vite";
 import tempest from "vite-plugin-tempest";
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
+
   plugins: [tailwindcss(), tempest()],
 });
