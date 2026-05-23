23aee34a3b27a4c4db6a15c3648f5ffa2c524de7
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2026-05-23 20:04:12 +0900

    format vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
-import tempest from 'vite-plugin-tempest'
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tempest from "vite-plugin-tempest";
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		tempest(),
-	],
-})
+  plugins: [tailwindcss(), tempest()],
+});
