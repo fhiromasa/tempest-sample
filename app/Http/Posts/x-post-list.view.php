64b7b7a3f56a1c8fce9325c4fbe95d3b701c8561
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2026-08-16 15:41:37 +0900

    rewrite view file foreach

--- app/Http/Posts/x-post-list.view.php
+++ app/Http/Posts/x-post-list.view.php
@@ -1,9 +1,13 @@
 <?php
+
+declare(strict_types=1);
+
 /** @var \App\Models\Posts[] $posts */
 ?>
 
 <div id="posts-container" class="posts-container">
-    <?php foreach ($posts as $post): ?>
-        <x-post :post="$post" />
-    <?php endforeach; ?>
+    <x-post :foreach="$posts as $post" />
+    <div :forelse>
+        Nothing here
+    </div>
 </div>
