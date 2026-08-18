25738a876ef10e7bbc08de4dd3b04e42d925cdcb
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2026-08-19 01:44:17 +0900

    set default page title.

--- app/ViewComponents/x-base.view.php
+++ app/ViewComponents/x-base.view.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 <!doctype html>
 <html lang="ja" class="h-dvh flex flex-col scroll-smooth">
 <head>
-    <title>{{ $title ?? 'Tempest' }}</title>
+    <title>{{ $title ?? 'PostHub - 投稿とコメント' }}</title>
 
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
